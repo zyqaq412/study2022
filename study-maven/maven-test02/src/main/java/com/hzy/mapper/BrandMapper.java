ccbeb0b807ba7dcd43c151716f2bd2b3892c8796
zxwyhzy <2791517764@qq.com>
2022-10-29 17:29:20 +0800

    mybatis 增删改查

--- study-maven/maven-test02/src/main/java/com/hzy/mapper/BrandMapper.java
+++ study-maven/maven-test02/src/main/java/com/hzy/mapper/BrandMapper.java
@@ -2,12 +2,20 @@ package com.hzy.mapper;
 
 import com.hzy.pojo.Brand;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.ResultMap;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 public interface BrandMapper {
+    @ResultMap("brandResultMap")
+    //简单sql 使用注解开发
+    @Select("select * from tb_brand")
     List<Brand> selectAll();
+    @ResultMap("brandResultMap")// 数据库列名和对象属性名不一样需要 映射注解
+    @Select("select * from tb_brand where id = #{id}")
     Brand selectByIdBrand(int id);
+
 
     //注解方式
     /*List<Brand> selectByStatusAndByBrandNameAndByCompanyName(
